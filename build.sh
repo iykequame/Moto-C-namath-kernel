dfdba61b18322abfd48e0917c93f0aee8b0f48ee
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-11-23 02:15:59 +0200

    Fix build script #2

--- build.sh
+++ build.sh
@@ -5,12 +5,5 @@
 
 mkdir -p out
 
-#Clone toolchain first: https://github.com/Mysteryagr/prebuilts-gcc-linux-x86-arm-cit-arm-eabi-4.8
-export CROSS_COMPILE=~/toolchain/prebuilts-gcc-linux-x86-arm-cit-arm-eabi-4.8/bin/arm-eabi-
-
-export ARCH=arm
-
-#Defconfig for Moto C
-make -C O=output ARCH=arm wt6737m_35_n_defconfig
-
-make -j4 -C ARCH=arm O=output
+make  O=output ARCH=arm CROSS_COMPILE="/$toolchain/prebuilts-gcc-linux-x86-arm-cit-arm-eabi-4.8/bin/arm-eabi-"    wt6737m_35_n_defconfig
+make -C  ./../Moto-C-namath-kernel/  ARCH=arm CROSS_COMPILE="$toolchain/prebuilts-gcc-linux-x86-arm-cit-arm-eabi-4.8/bin/arm-eabi-"   O=output 
